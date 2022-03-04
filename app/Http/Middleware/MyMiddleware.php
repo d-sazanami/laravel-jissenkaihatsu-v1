522404b99b9747e4514fd3111dfae0825c1c4d43
d-sazanami <dstrsazanami@gmail.com>
2022-03-04 13:51:06 +0900

    MyMiddlewareの利用

---
+++ app/Http/Middleware/MyMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Facades\MyService;
+use Closure;
+use Illuminate\Http\Request;
+
+class MyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $id = rand(0, count(MyService::alldata()));
+        MyService::setId($id);
+
+        $merge_data = [
+            'id' => $id,
+            'msg' => MyService::say(),
+            'alldata' => MyService::alldata()
+        ];
+        $request->merge($merge_data);
+
+        return $next($request);
+    }
+}
