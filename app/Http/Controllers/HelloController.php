aef2f8e74b8b5665059702e17f5951e870ee261f
d-sazanami <dstrsazanami@gmail.com>
2022-03-08 12:16:18 +0900

    idが偶数、ageが偶数のものを抽出

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,12 +11,15 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $msg = 'show people reord.';
-        $keys = Person::get()->modelKeys();
-        $even = array_filter($keys, function($key)
+        $even = Person::get()->filter(function($item)
         {
-            return $key % 2 == 0;
+            return $item->id % 2 == 0;
         });
-        $result = Person::get()->only($even);
+        $even2 = Person::get()->filter(function($item)
+        {
+            return $item->age % 2 == 0;
+        });
+        $result = $even->merge($even2);
 
         $data = [
             'msg' => $msg,
