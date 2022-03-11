fd4628604b2f5fcde8445d9291af0bfff818d321
d-sazanami <dstrsazanami@gmail.com>
2022-03-11 15:32:16 +0900

    データテスト実行

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,29 +14,14 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->get('/')->assertStatus(200);
+        $data = [
+            'id' => 1,
+            'name' => 'Reimu Hakurei',
+            'mail' => 'reimu@hakurei.com',
+            'age' => '16'
+        ];
+        $this->assertDatabaseHas('people', $data);
 
-        $this->get('/hello')->assertOK();
-        // $this->post('/hello')->assertOK();
-        // $this->get('/hello/1')->assertOK();
-        $this->get('/hoge')->assertStatus(404);
-
-        $this->get('/hello')->assertSeeText('Index');
-        //$this->get('/hello')->assertSee('<h1>');
-        // $this->get('/hello')->assertSeeInOrder(['<html', '<head', '<body', '<h1>']);
-
-        //$this->get('/hello/json/1')->assertSeeText('博麗霊夢');
-        /*
-        $this->get('/hello/json/2')->assertExactJson(
-            [
-                'id'=> 2,
-                'name' => '霧雨魔理沙',
-                'mail' => 'marisa@kirisame.jp',
-                'created_at' => null,
-                'updated_at' => null,
-                'age' => '16'
-            ]
-        );
-        */
+        $this->assertDatabaseMissing('people', ['id' => 14]);
     }
 }
