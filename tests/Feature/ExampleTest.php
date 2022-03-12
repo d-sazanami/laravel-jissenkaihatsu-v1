062b171a5a9b1169853a6362b456eee0a7ccda50
d-sazanami <dstrsazanami@gmail.com>
2022-03-12 09:19:52 +0900

    モデルを利用する

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -17,5 +18,28 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testBasicTest()
+    {
+        $data = [
+            'id' => 1,
+            'name' => 'DUMMY',
+            'mail' => 'dummy@mail',
+            'age' => 0
+        ];
+        $person = new Person();
+        $person->fill($data)->save();
+        $this->assertDatabaseHas('people', $data);
+
+        $person->name = 'NOT-DUMMY';
+        $person->save();
+        $this->assertDatabaseMissing('people', $data);
+
+        $data['name'] = 'NOT-DUMMY';
+        $this->assertDatabaseHas('people', $data);
+
+        $person->delete();
+        $this->assertDatabaseMissing('people', $data);
     }
 }
