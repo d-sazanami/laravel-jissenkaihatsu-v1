85c8947999cab8e714059b959126db049bae5435
d-sazanami <dstrsazanami@gmail.com>
2022-03-13 18:58:28 +0900

    コールバックの設定

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Person;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PersonFactory extends Factory
@@ -18,6 +19,17 @@ class PersonFactory extends Factory
             'mail' => $this->faker->unique()->safeEmail(),
             'age' => $this->faker->numberBetween(1, 100)
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterMaking(function (Person $person) {
+            $person->name .= '[making]';
+            $person->save();
+        })->afterCreating(function (Person $person) {
+            $person->name  .= '[creating]';
+            $person->save();
+        });
     }
 
     public function nameUpper()
