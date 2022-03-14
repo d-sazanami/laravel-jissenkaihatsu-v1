d5548869ff7141f801bb3762af1ddd2d5fd466ad
d-sazanami <dstrsazanami@gmail.com>
2022-03-14 09:23:10 +0900

    キューをテスト

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,11 @@
 namespace Tests\Feature;
 
 use App\Events\PersonEvent;
+use App\Jobs\MyJob;
+use App\Listeners\PersonEventListener;
 use App\Models\Person;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Events\CallQueuedListener;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
@@ -30,8 +34,20 @@ class ExampleTest extends TestCase
         Person::factory()->create();
         $person = Person::factory()->create();
 
-        Event::fake();
+        Queue::fake();
+        Queue::assertNothingPushed();
+
+        MyJob::dispatch($person->id);
+        Queue::assertPushed(MyJob::class);
+
+        // event(Person::class);
         $this->get('/hello/' . $person->id)->assertOk();
-        Event::assertDispatched(PersonEvent::class);
+        $this->get('/hello/' . $person->id)->assertOk();
+        Queue::assertPushed(CallQueuedListener::class, 2);
+        /*
+        Queue::assertPushed(function ($job) {
+            return $job->class == PersonEventListener::class;
+        });
+        */
     }
 }
